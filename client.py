95a156f4ebf79d6e0d19b66711330d56efce3d75
Santiago <santiago.dediego@tecnalia.com>
2023-03-15 11:49:41 +0100

    Fixed bug

--- client.py
+++ client.py
@@ -25,9 +25,10 @@ def init(props):
 def executeChallenge():
     print("Python: starting executeChallenge()")
     payload={'reading_id':random.randint(0,100),'ip':'192.168.0.4','data':'1023'}
-    key=requests.post("http://localhost:5000/readings",params=payload)
+    response=requests.post("http://localhost:5000/readings",params=payload)
+    print(response.json())
     # Get key as UTF-8 and calculate its length
-    key = bytes(key, 'utf-8')
+    key = bytes(response.json(), 'utf-8')
     key_size = len(key)
 
     # The result is a tuple (key, key_size)
